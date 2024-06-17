c5b0df7171f3ce973b2dc8a7cc255d3b1af46b88
siennahuang <siennahuang@tencent.com>
2024-06-17 17:36:13 +0800

    feat: hashutil

--- annotation.go
+++ annotation.go
@@ -12,12 +12,12 @@ import (
 	"strings"
 )
 
-//GetBucketNum 获取 bucket num
+//GetBucketNum get bucket num
 func GetBucketNum(hashMethod protoc_cache_server.HashMethod, source string, seed int64, bucketSize int64) int64 {
 	return int64(GetHashNum(hashMethod, source, uint64(seed)))%bucketSize + 1
 }
 
-// GetHashNum 获取 hash 值
+// GetHashNum get hash
 func GetHashNum(hashMethod protoc_cache_server.HashMethod, source string, seed uint64) uint64 {
 	switch hashMethod {
 	// APHash
@@ -33,22 +33,22 @@ func GetHashNum(hashMethod protoc_cache_server.HashMethod, source string, seed u
 	case protoc_cache_server.HashMethod_HASH_METHOD_NEW_MD5:
 		return NewMD5(source)
 	default:
-		// 默认 BKDRHash
+		// default BKDRHash
 		return BKDR(source, seed)
 	}
 }
 
-// BKDRHash BKDRHash实现方法
-// @param str 用户标识
-// @param seed 素数种子
-// @return uint64 hash值
+// BKDRHash BKDRHash Implementation Method
+// @param str User ID
+// @param seed Prime number seed
+// @return uint64 hash
 func BKDR(source string, seed uint64) uint64 {
 	// algorithm invented by Brian Kernighan, Dennis Ritchie.
 	var hash uint64 = 0
 	i := 0
 	var len = uint64(len(source))
 	var j uint64 = 0
-	// BKDR算法无论是在实际效果还是编码实现中，效果都是最突出的
+	// The BKDR algorithm has the most outstanding effect both in actual effect and coding implementation.
 	for j < len {
 		hash = hash*seed + uint64(source[j])
 		j++
@@ -62,18 +62,18 @@ func BKDR(source string, seed uint64) uint64 {
 		}
 		i++
 	}
-	// 得到hash值后，再配合层域桶数进行取余操作
+	// After obtaining the hash value, perform the remainder operation in conjunction with the number of layer domain buckets.
 	return hash & 0x7FFFFFFF
 }
 
-// APHash APHash实现方法
-// @param str 用户标识
-// @return uint64 hash值
+// APHash APHash
+// @param str User ID
+// @return uint64 hash
 func AP(source string) uint64 {
 	var l = len(source)
 	var hash uint64 = 0
 	i := 0
-	// APHash也是较为优秀的Hash算法
+	// APHash is also a relatively good hash algorithm
 	for i < l {
 		if (i & 1) == 0 {
 			hash ^= (hash << 7) ^ uint64(source[i]) ^ (hash >> 3)
@@ -85,27 +85,27 @@ func AP(source string) uint64 {
 	return hash & 0x7FFFFFFF
 }
 
-// DJBHash DJBHash实现方法
-// @param str 用户标识
-// @return uint64 hash值
+// DJBHash DJBHash
+// @param str User ID
+// @return uint64 hash
 func DJB(source string) uint64 {
 	var l = len(source)
 	i := 0
-	// 初始值是5381，遍历整个串，按照hash * 33 +c的算法计算。得到的结果就是哈希值
-	// 经过大量实验，发现5381和33的结果分散小
-	// 5381和33这两个数字可以让哈希结果分散
+// The initial value is 5381. Traverse the entire string and calculate according to the algorithm of hash * 33 +c. The result is the hash value
+// After a lot of experiments, it is found that the results of 5381 and 33 are less dispersed
+// The two numbers 5381 and 33 can make the hash results dispersed
 	var hash uint64 = 5381
 	for i < l {
-		// 乘33的操作用左移和加法实现，提高性能
+		// The multiplication by 33 is implemented by left shift and addition to improve performance
 		hash += (hash << 5) + uint64(source[i])
 		i++
 	}
 	return hash & 0x7FFFFFFF
 }
 
-// HashNew 新hash实现方法
-// @param str 用户标识
-// @return uint64 值
+// HashNew
+// @param str User ID
+// @return uint64
 func New(source string) uint64 {
 	var value uint32 = 0
 	var l = len(source)
@@ -123,14 +123,14 @@ func New(source string) uint64 {
 	return uint64(value)
 }
 
-// hashNewMD5 md5情况 hashType
-// @param guid 用户标识
-// @return uint64 md5值
+// hashNewMD5 md5 hashType
+// @param guid User ID
+// @return uint64 md5
 func NewMD5(source string) uint64 {
 	h := md5.New()
 	h.Write([]byte(source)) // nolint
 	cipherStr := h.Sum(nil)
-	return uint64(New(strings.ToUpper(hex.EncodeToString(cipherStr)))) // md5情况
+	return uint64(New(strings.ToUpper(hex.EncodeToString(cipherStr)))) // md5
 }
 
 */
